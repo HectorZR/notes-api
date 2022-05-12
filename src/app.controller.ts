4660ca2c55e32337632e14e3b52a4588007e9171
Hector Zurga <hjzurga@gmail.com>
2022-05-11 22:03:40 -0400

    Database connected

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('ping')
+  getPing(): Record<string, boolean | string> {
+    return {
+      isActive: true,
+      time: new Date().toISOString(),
+    };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DATABASE_NAME,
+      password: process.env.DATABASE_PASSWORD,
+      username: process.env.DATABASE_USER,
+      port: Number(process.env.DATABASE_PORT),
+      host: process.env.DATABASE_HOST,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
