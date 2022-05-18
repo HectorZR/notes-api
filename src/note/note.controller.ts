8d789f7277edfad429f944038c3ca664ef5616f8
Hector Zurga <hjzurga@gmail.com>
2022-05-17 21:15:20 -0400

    Added all crud endpoints

--- src/note/note.controller.ts
+++ src/note/note.controller.ts
@@ -6,17 +6,23 @@ import {
   Patch,
   Param,
   Delete,
+  BadRequestException,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { NoteService } from './note.service';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
 
-@Controller('note')
+@Controller('notes')
 export class NoteController {
   constructor(private readonly noteService: NoteService) {}
 
   @Post()
   create(@Body() createNoteDto: CreateNoteDto) {
+    if (!createNoteDto.content)
+      throw new BadRequestException('content value is required');
+
     return this.noteService.create(createNoteDto);
   }
 
@@ -32,10 +38,14 @@ export class NoteController {
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
+    if (!updateNoteDto.content)
+      throw new BadRequestException('content value is required');
+
     return this.noteService.update(+id, updateNoteDto);
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
     return this.noteService.remove(+id);
   }
--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -1,26 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Note } from '../entity/Note';
 import { CreateNoteDto } from './dto/create-note.dto';
+import { NoteDto } from './dto/note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
 
 @Injectable()
 export class NoteService {
-  create(createNoteDto: CreateNoteDto) {
-    return 'This action adds a new note';
+  columnsToReturn = ['id', 'content', 'updatedAt', 'createdAt'];
+  constructor(
+    @InjectRepository(Note)
+    private noteRepository: Repository<Note>,
+  ) {}
+
+  async create(createNoteDto: CreateNoteDto) {
+    const timestamp = new Date();
+
+    return (
+      await this.noteRepository
+        .createQueryBuilder()
+        .insert()
+        .into(Note)
+        .values({
+          ...createNoteDto,
+          createdAt: timestamp,
+          updatedAt: timestamp,
+        })
+        .returning(this.columnsToReturn)
+        .execute()
+    ).raw[0];
   }
 
   findAll() {
-    return `This action returns all note`;
+    return this.noteRepository
+      .createQueryBuilder()
+      .addOrderBy('"createdAt"', 'DESC')
+      .getMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} note`;
+  async findOne(id: number) {
+    const note = await this.noteRepository.findOne({ where: { id } });
+
+    if (!note) throw new NotFoundException(`Note with id: ${id} not found`);
+
+    return note;
   }
 
-  update(id: number, updateNoteDto: UpdateNoteDto) {
-    return `This action updates a #${id} note`;
+  async update(id: number, updateNoteDto: UpdateNoteDto) {
+    await this.findOne(id);
+
+    return (
+      await this.noteRepository
+        .createQueryBuilder()
+        .update(Note)
+        .set({ ...updateNoteDto, updatedAt: new Date() })
+        .where({ id })
+        .returning(this.columnsToReturn)
+        .execute()
+    ).raw[0];
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} note`;
+  async remove(id: number) {
+    await this.findOne(id);
+    return this.noteRepository
+      .createQueryBuilder()
+      .delete()
+      .from(Note)
+      .where({ id })
+      .execute();
   }
 }
