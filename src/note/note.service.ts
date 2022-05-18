a4dc63978f477d37d2f08e202cbb4fe059d6d64b
Hector Zurga <hjzurga@gmail.com>
2022-05-17 21:19:14 -0400

    Fixed error

--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -3,7 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Note } from '../entity/Note';
 import { CreateNoteDto } from './dto/create-note.dto';
-import { NoteDto } from './dto/note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
 
 @Injectable()
