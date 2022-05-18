1b175c61ff07f7f9a20ab026828499da64cc2bca
Hector Zurga <hjzurga@gmail.com>
2022-05-17 22:52:42 -0400

    adding table

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,41 @@ import { AppDataSource } from './data-source';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await AppDataSource.initialize();
+  AppDataSource.initialize().then(async (dataSource) => {
+    await dataSource.createQueryRunner().createTable({
+      name: 'notes',
+      columns: [
+        // @ts-expect-error only required properties are defined
+        {
+          name: 'id',
+          isPrimary: true,
+          isUnique: true,
+          type: 'int',
+          isGenerated: true,
+          isNullable: false,
+        },
+        // @ts-expect-error only required properties are defined
+        {
+          name: 'content',
+          type: 'varchar',
+          isNullable: false,
+          length: '2000',
+        },
+        // @ts-expect-error only required properties are defined
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          isNullable: false,
+        },
+        // @ts-expect-error only required properties are defined
+        {
+          name: 'updated_at',
+          type: 'timestamp',
+          isNullable: true,
+        },
+      ],
+    });
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
